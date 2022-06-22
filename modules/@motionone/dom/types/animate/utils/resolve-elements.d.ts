03d453d9bb1a9c3ebe715a4c97c4d9e1d39e827d
jedd-s <not.jedd@gmail.com>
2022-06-21 20:37:24 -0700

    ...

---
+++ modules/@motionone/dom/types/animate/utils/resolve-elements.d.ts
@@ -0,0 +1,5 @@
+import type { AcceptedElements } from "../types";
+export declare function resolveElements(elements: AcceptedElements, selectorCache?: {
+    [key: string]: NodeListOf<Element>;
+}): Element[];
+//# sourceMappingURL=resolve-elements.d.ts.map
---
+++ modules/@motionone/dom/types/state/gestures/in-view.d.ts
@@ -0,0 +1,6 @@
+import { Gesture } from "./types";
+/**
+ * TODO: Support viewport options
+ */
+export declare const inView: Gesture;
+//# sourceMappingURL=in-view.d.ts.map
